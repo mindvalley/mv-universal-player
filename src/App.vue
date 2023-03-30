a25df10d51e36bb1b43f7c3cd44fd517b3332b52
amitchaudhary140 <amit@mindvalley.com>
2023-03-30 15:56:15 +0530

    create state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>App Component</div>
-  <AudioPlayer id="amit" v-slot="{ player }">
+  <AudioPlayer id="amit" v-slot="{ player, state }" @play="play1" @pause="paused1">
+    {{ state }}
     <button @click="player.play">Play</button>
     <button @click="player.pause">Pause</button>
   </AudioPlayer>
@@ -8,4 +9,14 @@
 
 <script setup lang="ts">
 import AudioPlayer from './components/Audio/AudioPlayer.vue'
+
+const play1 = (event?: any) => {
+  console.log('play clicked')
+  console.log(event)
+}
+
+const paused1 = (event?: any) => {
+  console.log('pause clicked')
+  console.log(event)
+}
 </script>
--- src/components/Audio/AudioPlayer.vue
+++ src/components/Audio/AudioPlayer.vue
@@ -1,13 +1,94 @@
 <script setup lang="ts">
-import { stringify } from 'querystring'
 import VideoJsPlayer from 'video.js'
-// import type { VideoJsPlayer as Player } from 'video.js'
-import { onMounted, onUnmounted, reactive, ref } from 'vue'
-export interface Props {}
+import { onMounted, onUnmounted, ref, defineEmits, watch } from 'vue'
 
-export interface Source {
+interface Source {
   type: string
   src: string
+}
+const StateConfig = {
+  src: {
+    getter: (player: any) => player.src()
+  },
+  currentSrc: {
+    getter: (player: any) => player.currentSrc()
+  },
+  currentSource: {
+    getter: (player: any) => player.currentSource()
+  },
+  volume: {
+    events: ['volumechange'],
+    getter: (player: any) => player.volume()
+  },
+  muted: {
+    events: ['volumechange'],
+    getter: (player: any) => player.muted()
+  },
+  seeking: {
+    events: ['seeking'],
+    getter: (player: any) => player.seeking()
+  },
+  paused: {
+    events: ['pause', 'play', 'playing'],
+    getter: (player: any) => player.paused()
+  },
+  ended: {
+    events: ['ended', 'play'],
+    getter: (player: any) => player.ended()
+  },
+  currentTime: {
+    events: ['timeupdate'],
+    getter: (player: any) => player.currentTime()
+  },
+  duration: {
+    events: ['durationchange'],
+    getter: (player: any) => player.duration()
+  },
+  playbackRate: {
+    events: ['ratechange'],
+    getter: (player: any) => player.playbackRate()
+  },
+  playbackRates: {
+    events: ['playbackrateschange'],
+    getter: (player: any) => player.playbackRates()
+  },
+  readyState: {
+    events: ['loadeddata'],
+    getter: (player: any) => player.readyState()
+  },
+  networkState: {
+    events: ['loadeddata', 'error'],
+    getter: (player: any) => player.networkState()
+  },
+  error: {
+    events: ['loadeddata', 'error'],
+    getter: (player: any) => player.error()
+  },
+  buffered: {
+    events: ['progress'],
+    getter: (player: any) => player.buffered()
+  },
+  bufferedPercent: {
+    events: ['progress'],
+    getter: (player: any) => player.bufferedPercent()
+  },
+  played: {
+    events: ['timeupdate'],
+    getter: (player: any) => player.played()
+  },
+  seekable: {
+    events: ['progress', 'seeked'],
+    getter: (player: any) => player.seekable()
+  }
+}
+
+type PlayerBaseState = {
+  [K in keyof typeof StateConfig]: ReturnType<(typeof StateConfig)[K]['getter']>
+}
+
+interface PlayerState extends PlayerBaseState {
+  playing: boolean
+  waiting: boolean
 }
 
 const props = defineProps({
@@ -30,6 +111,49 @@ const props = defineProps({
 })
 
 let audioInstance: any
+let state = ref({} as PlayerState)
+const emit = defineEmits(['play', 'pause', 'timeupdate', 'seeking', 'ended', 'ready'])
+
+watch(
+  () => state.value.playing == true,
+  (playing) => {
+    if (playing) {
+      emit('play')
+    }
+  }
+)
+
+watch(
+  () => state.value.paused,
+  (paused) => {
+    if (paused) {
+      emit('pause')
+    }
+  }
+)
+
+watch(
+  () => state.value.currentTime,
+  (currentTime) => {
+    emit('timeupdate', { currentTime: currentTime })
+  }
+)
+
+watch(
+  () => state.value.seeking,
+  (seeking) => {
+    emit('seeking', { seeking: seeking })
+  }
+)
+
+watch(
+  () => state.value.ended,
+  (ended) => {
+    if (ended) {
+      emit('ended')
+    }
+  }
+)
 
 onMounted(() => {
   initialize()
@@ -44,8 +168,51 @@ onUnmounted(() => {
 })
 
 const initialize = () => {
-  audioInstance = createInstance(props.id, { controls: false, playbackRates: props.playbackRates })
+  audioInstance = createInstance(props.id, {
+    controls: false,
+    playbackRates: props.playbackRates
+  })
   setSources(props.sources)
+  createState()
+
+  emit('ready')
+}
+
+const createState = () => {
+  const keys = Object.keys(StateConfig) as Array<keyof typeof StateConfig>
+  state.value = keys.reduce(
+    (result, key) => ({ ...result, [key]: StateConfig[key].getter(audioInstance) }),
+    {
+      playing: false,
+      waiting: false
+    } as any
+  )
+
+  // playing state https://github.com/videojs/video.js/issues/181
+  // un-loop: play > playing > pause > ended
+  // loop: play > playing > [ended > restart] waiting > playing
+  audioInstance.on(['pause', 'ended'], () => {
+    updateState('playing', false)
+  })
+  audioInstance.on(['play', 'playing'], () => {
+    updateState('playing', true)
+  })
+
+  keys.forEach((key) => {
+    const target = StateConfig[key]
+    const baseEvents = ['loadstart', 'loadedmetadata']
+    audioInstance.on(baseEvents.concat((target as any).events ?? []), () => {
+      console.log('fire ---')
+      updateState(key, target.getter(audioInstance))
+    })
+  })
+
+  console.log('----state -----')
+  console.log(state)
+}
+
+const updateState = (key: keyof PlayerState, value: any) => {
+  state.value[key] = value as never
 }
 
 const createInstance = (id: string, options: any) => {
@@ -63,6 +230,7 @@ const setSources = (sources: Source[]) => {
 const play = () => {
   if (audioInstance) {
     audioInstance.play()
+    emit('play', 'hello play')
   }
 }
 
@@ -107,7 +275,7 @@ const player = {
         <a href="https://videojs.com/html5-video-support/" target="_blank"> supports HTML5 video</a>
       </p>
     </video>
-    <slot :player="player"></slot>
+    <slot :player="player" :state="state"></slot>
   </div>
 </template>
 
