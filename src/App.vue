f7176d8d6bd1a396a5d3f1a11067e235cb4e4ec0
amitchaudhary140 <amit@mindvalley.com>
2023-04-11 16:15:28 +0530

    add vue carousel

--- src/App.vue
+++ src/App.vue
@@ -32,18 +32,36 @@
               <div class="text-cool-grey-350 mb-2 text-xs">Mix Track</div>
 
               <div class="flex gap-x-2">
-                <MeditationTrackItem
-                  :volume="0"
-                  @click="isVolumeSliderDisabled = true"
-                ></MeditationTrackItem>
-                <MeditationTrackItem
-                  :volume="volume"
-                  @click="isVolumeSliderDisabled = false"
-                  v-for="(sound, index) in backgroundSounds"
-                  :key="index"
-                  :sources="formatSources(sound?.item?.mediaAsset.renditions)"
-                  :background-src="sound?.item?.coverAsset?.thumbnailUrl"
-                />
+                <Carousel
+                  data-testid="carousel"
+                  :items-to-show="7"
+                  :items-to-scroll="1"
+                  :settings="settings"
+                  :breakpoints="breakpoints"
+                >
+                  <Slide key="no-bg-sound">
+                    <MeditationTrackItem
+                      :volume="0"
+                      @click="isVolumeSliderDisabled = true"
+                    ></MeditationTrackItem>
+                  </Slide>
+                  <Slide
+                    data-testid="slide"
+                    v-for="(sound, index) in backgroundSounds"
+                    :key="index"
+                  >
+                    <MeditationTrackItem
+                      :volume="volume"
+                      @click="isVolumeSliderDisabled = false"
+                      :key="index"
+                      :sources="formatSources(sound?.item?.mediaAsset.renditions)"
+                      :background-src="sound?.item?.coverAsset?.thumbnailUrl"
+                    />
+                  </Slide>
+                  <template #addons>
+                    <Navigation data-testid="navigation" />
+                  </template>
+                </Carousel>
               </div>
               <div
                 class="flex w-full mt-4 items-center justify-center transition duration-300 ease-in"
@@ -83,6 +101,8 @@ import type { Source } from './types/audio'
 import MeditationTrackItem from './components/audio/Meditation/MeditationTrackItem/MeditationTrackItem.vue'
 import MeditationVolumeSlider from './components/audio/Meditation/MeditationVolumeSlider/MeditationVolumeSlider.vue'
 import { ref } from 'vue-demi'
+import { Carousel, Navigation, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 
 const isVolumeSliderDisabled = ref(true)
 const volume = ref(0.5)
@@ -105,4 +125,50 @@ const formatSources = (localSources: Array<any> = []) => {
 
   return updatedSources
 }
+
+const settings = {
+  // Mobile (Default)
+  itemsToShow: 4,
+  snapAlign: 'start'
+}
+// breakpoints are mobile first
+// any settings not specified will fallback to the carousel settings
+const breakpoints = {
+  //   540 and up
+  540: {
+    itemsToShow: 6,
+    snapAlign: 'start'
+  },
+  //   640px (sm) and up
+  640: {
+    itemsToShow: 8,
+    snapAlign: 'start'
+  },
+  // 1024 (lg) and up
+  1024: {
+    itemsToShow: 7,
+    snapAlign: 'start'
+  }
+}
 </script>
+
+<style>
+.carousel__prev,
+.carousel__next {
+  color: white !important;
+}
+
+.carousel__prev {
+  left: -30px;
+}
+
+.carousel__next {
+  right: -30px;
+}
+
+.carousel__prev--disabled,
+.carousel__next--disabled {
+  cursor: not-allowed;
+  opacity: 0.2;
+}
+</style>
--- src/components/audio/AudioResource/AudioResource.vue
+++ src/components/audio/AudioResource/AudioResource.vue
@@ -52,7 +52,7 @@
         <div class="col-span-3 md:col-span-2 flex">
           <!-- Controls -->
           <div
-            class="w-full shrink grow text-cool-grey-250 md:flex md:max-w-[620px] md:flex-col md:items-center md:justify-center"
+            class="w-full shrink grow text-cool-grey-250 md:flex md:flex-col md:items-center md:justify-center"
           >
             <!-- Medium-screen above meta -->
             <section class="w-full py-4 hidden md:block">
