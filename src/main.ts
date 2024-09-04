7b711b90e124a52a75caf4e82a00bd2316b9eee7
amitchaudhary140 <amit@mindvalley.com>
2024-09-04 11:57:54 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import svgSprite from '@mindvalley/design-system/dist/svg-sprite/svg-defs.svg'
 import SvgSprite from 'vue-svg-sprite'
 import './../public/assets/main.css'
 
-Vue.use(SvgSprite, {
+const app = createApp(App)
+
+app.use(SvgSprite, {
   url: svgSprite
 })
 
-new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+app.mount('#app')
