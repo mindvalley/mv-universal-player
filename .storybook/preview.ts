68259aa86ae22fb1cf631dcec456f635c2e0025d
amitchaudhary140 <amit@mindvalley.com>
2024-09-10 16:05:28 +0530

    add soundscape story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import { Preview, setup } from '@storybook/vue3'
 import './../public/assets/main.css'
 import svgSprite from '@mindvalley/design-system/dist/svg-sprite/svg-defs.svg'
 import { svgSpriteDirectivePlugin } from 'vue-svg-sprite'
+import { tooltip } from './../src/directives/tooltip'
 
 const preview: Preview = {
   parameters: {
@@ -18,6 +19,7 @@ setup((app) => {
   app.use(svgSpriteDirectivePlugin, {
     url: svgSprite
   })
+  app.directive('tooltip', tooltip)
 })
 
 export default preview
