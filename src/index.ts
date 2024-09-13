a565fe6b43df45fba521e4785a6656099f421f90
amitchaudhary140 <amit@mindvalley.com>
2024-09-13 12:25:29 +0530

    fix svg icons import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as components from './components'
 import './assets/main.css'
-import svgSprite from '@mindvalley/design-system/dist/svg-sprite/svg-defs.svg'
+import svgSprite from '@mindvalley/design-system/dist/icons/sprites/ui-icons.svg'
 import { svgSpriteDirectivePlugin } from 'vue-svg-sprite'
 
 import type { App } from 'vue-demi'
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import svgSprite from '@mindvalley/design-system/dist/svg-sprite/ui-icons.svg'
+import svgSprite from '@mindvalley/design-system/dist/icons/sprites/ui-icons.svg'
 import { svgSpriteDirectivePlugin } from 'vue-svg-sprite'
 import './assets/main.css'
 import { tooltip } from './directives/tooltip'
