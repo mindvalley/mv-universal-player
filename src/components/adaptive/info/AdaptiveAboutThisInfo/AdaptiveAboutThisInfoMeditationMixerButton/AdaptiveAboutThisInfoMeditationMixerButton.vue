107dcf85e302a31f42822bf3462bfe7634cbb817
amitchaudhary140 <amit@mindvalley.com>
2024-11-04 10:30:54 +0530

    add hover state in about this info controls

--- src/components/adaptive/info/AdaptiveAboutThisInfo/AdaptiveAboutThisInfoMeditationMixerButton/AdaptiveAboutThisInfoMeditationMixerButton.vue
+++ src/components/adaptive/info/AdaptiveAboutThisInfo/AdaptiveAboutThisInfoMeditationMixerButton/AdaptiveAboutThisInfoMeditationMixerButton.vue
@@ -16,18 +16,18 @@ const props = defineProps({
 <template>
   <button
     data-testid="adaptive-about-this-info-meditation-mixer-button"
-    class="flex flex-row w-full pl-4 pr-2 text-teal-300 bg-white-12a border border-white-12a rounded-lg items-center"
+    class="flex flex-row w-full pl-4 pr-2 text-teal-300 bg-white-12a hover:bg-white-24a border border-white-12a rounded-lg items-center"
     :class="[mixerEnabled ? 'py-2' : 'py-3']"
   >
     <span data-testid="mixer-icon" class="flex items-center mr-2">
       <svg v-svg symbol="sliders-outlined" class="h-4 w-4"></svg>
     </span>
-    <span data-testid="mixer-available" v-if="!mixerEnabled" class="title-9 flex-grow text-start"
+    <span data-testid="mixer-available" v-if="!mixerEnabled" class="heading-9 flex-grow text-start"
       >Mixer available</span
     >
     <div data-testid="mixer-mixing" v-else class="flex-grow overflow-hidden">
       <AdaptiveMarqueeContent>
-        <span class="title-9 mr-12">Mixing with {{ trackTitle }}</span>
+        <span class="heading-9 mr-12">Mixing with {{ trackTitle }}</span>
       </AdaptiveMarqueeContent>
     </div>
     <span data-testid="mixer-chevron" class="ml-2">
--- src/components/adaptive/info/AdaptiveAboutThisInfo/AdaptiveAboutThisInfoSetDurationButton/AdaptiveAboutThisInfoSetDurationButton.vue
+++ src/components/adaptive/info/AdaptiveAboutThisInfo/AdaptiveAboutThisInfoSetDurationButton/AdaptiveAboutThisInfoSetDurationButton.vue
@@ -3,11 +3,11 @@
 <template>
   <button
     data-testid="adaptive-about-this-info-set-duration-button"
-    class="flex flex-row w-full py-3 px-2 text-teal-300 bg-white-12a border border-white-12a rounded-lg items-center justify-between"
+    class="flex flex-row w-full py-3 px-2 text-teal-300 bg-white-12a hover:bg-white-24a border border-white-12a rounded-lg items-center justify-between"
   >
     <span data-testid="set-duration-icon" class="flex flex-row ml-2">
       <span><svg v-svg symbol="stopwatch-outlined" class="h-4 w-4"></svg></span
-      ><span data-testid="set-duration-text" class="title-9 ml-[6px]">Set duration</span>
+      ><span data-testid="set-duration-text" class="heading-9 ml-[6px]">Set duration</span>
     </span>
     <span data-testid="set-duration-chevron">
       <svg v-svg symbol="chevron-right-outlined" class="h-4 w-4 text-white"></svg>
